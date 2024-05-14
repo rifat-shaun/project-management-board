7a6240e20ece7dac7f4001bf7202ce2af618df33
rifat-shaun <mdrifathasanshaun@gmail.com>
2024-05-15 02:50:48 +0600

    added folders and pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Home } from "./pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <Home />
     </div>
   );
 }
